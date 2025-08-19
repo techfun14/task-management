fd4be0934e77720c3319afa4070f05218b02203c
Utkarsh Jain <jainutkarshuj97@gmail.com>
2025-08-20 00:52:42 +0530

    Utkarsh

---
+++ TaskMangementBackend/TaskManagement/src/main/java/com/example/TaskManagement/UsersPackage/UserJpaController.java
@@ -0,0 +1,33 @@
+package com.example.TaskManagement.UsersPackage;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserJpaController {
+    private UserRepository userRepository;
+
+    public UserJpaController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @PostMapping("/signup")
+    public ResponseEntity<?> createUser(@RequestBody User user){
+       if(userRepository.existsByUserName(user.getUserName())){
+           return ResponseEntity.badRequest().body("UserName Already Exists");
+
+       }
+       if(userRepository.existsByEmailAddress(user.getEmailAddress())){
+           return ResponseEntity.badRequest().body("Email Already Exists");
+       }
+       userRepository.save(user);
+       return ResponseEntity.ok("User is Created");
+    }
+    @GetMapping
+    public ResponseEntity<?> getAllUsers(){
+        return ResponseEntity.ok(userRepository.findAll());
+    }
+
+}
